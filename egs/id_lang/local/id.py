b5bd675edd5213e69401a6ff951202824e679353
Airenas Vaiciunas <airenass@gmail.com>
2025-05-16 14:05:43 +0300

    feat: update Makefiles for new runner command and add language detection functionality

--- egs/id_lang/local/id.py
+++ egs/id_lang/local/id.py
@@ -1,39 +1,170 @@
+import argparse
+import io
+import json
+import math
 import os
+import re
+import sys
+
 import torchaudio
-from speechbrain.inference.classifiers import EncoderClassifier
 
-# Load the pretrained language ID model
-language_id = EncoderClassifier.from_hparams(
-    source="speechbrain/lang-id-voxlingua107-ecapa",
-    savedir="tmp/lang-id"
-)
+from preparation.logger import logger
 
-EXPECTED_LANG = "lt"
-CONFIDENCE_THRESHOLD = 0.80  
 
-AUDIO_DIR = "/home/airenas/projects/training-asr-nxt/tests"
-
-# Helper to analyze one file
-def check_if_lt(audio_path):
+def detect_lang(language_id, audio_buffer):
     try:
-        signal = language_id.load_audio(audio_path)
+        # signal = language_id.load_audio(audio_path)
+        signal, sample_rate = torchaudio.load(audio_buffer)
         prediction = language_id.classify_batch(signal)
         _, log_prob_target, _, predicted_lang = prediction
-
         predicted = predicted_lang[0]
         confidence = log_prob_target.exp().item()
-
-        if predicted.startswith(EXPECTED_LANG) and confidence > CONFIDENCE_THRESHOLD:
-            status = "✅ LT"
-        else:
-            status = "❌ Not LT or low confidence"
-
-        print(f"{os.path.basename(audio_path):30} -> {predicted:6} | Confidence: {confidence:.2f} | {status}")
-
+        return predicted, confidence
     except Exception as e:
-        print(f"{audio_path}: ERROR - {e}")
+        logger.error(f"{audio_buffer}: ERROR - {e}")
+        return None, 0.0
 
-# Process all audio files in the directory
-for file in os.listdir(AUDIO_DIR):
-    if file.endswith(".m4a"):  # or change to .m4a if using ffmpeg support
-        check_if_lt(os.path.join(AUDIO_DIR, file))
+
+def extracted_audio(waveform, sample_rate, segment):
+    start_sample = int(segment.start * sample_rate)
+    end_sample = int(segment.end * sample_rate)
+    part = waveform[:, start_sample:end_sample]
+
+    buffer = io.BytesIO()
+    torchaudio.save(buffer, part, sample_rate, format="wav")
+    buffer.seek(0)
+    return buffer
+
+
+def detect_language(audio_path, segments):
+    from speechbrain.inference.classifiers import EncoderClassifier
+
+    language_id = EncoderClassifier.from_hparams(
+        source="speechbrain/lang-id-voxlingua107-ecapa",
+        savedir="tmp/lang-id"
+    )
+
+    waveform, sample_rate = torchaudio.load(audio_path)
+    res = []
+    for segment in segments:
+        part = extracted_audio(waveform, sample_rate, segment)
+
+        lang, conf = detect_lang(language_id, part)
+        logger.info(f"Detected language: {lang} with confidence {conf}")
+        res += [LangRes(segment=segment, lang=lang, conf=conf)]
+    return res
+
+
+class Segment:
+    def __init__(self, label, start, end):
+        self.label = label
+        self.start = start
+        self.end = end
+
+    def __repr__(self):
+        return f"Segment(label={self.label}, start={self.start}, end={self.end})"
+
+    def len(self):
+        return self.end - self.start
+
+    def __eq__(self, other):
+        if not isinstance(other, Segment):
+            return False
+        return self.label == other.label and math.isclose(self.start, other.start) and math.isclose(self.end, other.end)
+
+    def to_dict(self):
+        return {
+            "label": self.label,
+            "start": self.start,
+            "end": self.end
+        }
+
+
+class LangRes:
+    def __init__(self, segment, lang, conf):
+        self.segment = segment
+        self.lang = lang
+        self.conf = conf
+
+    def to_dict(self):
+        return {
+            "segment": self.segment.to_dict(),
+            "lang": self.lang,
+            "conf": self.conf
+        }
+
+    def __repr__(self):
+        return f"LangRes(segment={self.segment}, lang={self.lang}, conf={self.conf})"
+
+
+def find_segment(speech_segments, start, min_len):
+    to = start + min_len
+    for s in speech_segments:
+        if s.len() < min_len:
+            continue
+        if s.start >= start:
+            return Segment(s.label, s.start, s.start + min_len)
+        if s.end >= to:
+            return Segment(s.label, s.end - min_len, s.end)
+        if s.start < start and s.end > to:
+            return Segment(s.label, start, to)
+    return None
+
+
+def select_test_segments(segments, num_segments=5, min_len=5):
+    speech_segments = [seg for seg in segments if seg.label == "speech"]
+    total_duration = sum(s.end - s.start for s in speech_segments)
+    logger.info(f"total speech duration: {total_duration}")
+    parts_from = total_duration / num_segments + 1
+    res = []
+    for i in range(num_segments):
+        start = i * parts_from
+        s = find_segment(speech_segments, start, min_len)
+        if s:
+            if s not in res:
+                res.append(s)
+    return res
+
+
+def load_segments(input_file):
+    dir_name = os.path.dirname(input_file)
+    segments_file = os.path.join(dir_name, "audio.ina_segments")
+
+    try:
+        with open(segments_file, 'r', encoding='utf-8') as f:
+            content = f.read()
+        segments = re.findall(r'\["(.*?)", ([\d.]+), ([\d.]+)\]', content)
+        parsed_segments = [Segment(label, float(start), float(end)) for label, start, end in segments]
+        return parsed_segments
+    except Exception as e:
+        raise RuntimeError(f"Failed to load segments from {segments_file}: {e}")
+
+
+def main(argv):
+    logger.info("Starting")
+    parser = argparse.ArgumentParser(description="Runs audio processing")
+    parser.add_argument("--input", nargs='?', required=True, help="File")
+    parser.add_argument("--output", nargs='?', required=True, help="File")
+
+    args = parser.parse_args(args=argv)
+
+    logger.info(f"Input file   : {args.input}")
+    logger.info(f"Output file  : {args.output}")
+    segments = load_segments(args.input)
+    # logger.info(f"Got segments: {segments}")
+    for sl in [8, 5, 3, 1]:
+        test_segments = select_test_segments(segments, num_segments=5, min_len=sl)
+        if len(test_segments) > 1:
+            break
+    if len(test_segments) == 0:
+        raise RuntimeError(f"Failed to find test segments in {args.input}")
+    logger.info(f"Made test segments: {test_segments}")
+    res = detect_language(args.input, test_segments)
+    logger.info(f"Got results: {res}")
+    with open(args.output, "w") as f:
+        for r in res:
+            f.write(json.dumps(r.to_dict(), ensure_ascii=False) + "\n")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
