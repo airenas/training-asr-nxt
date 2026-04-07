d3bc699c3aa71ade733a4321115b0bd487858b53
Airenas Vaiciunas <airenass@gmail.com>
2026-04-07 15:00:50 +0300

    fix: copy files - save as tar

--- egs/analysis/local/copy_audio.py
+++ egs/analysis/local/copy_audio.py
@@ -2,8 +2,12 @@ import argparse
 import json
 import os
 import sys
+import tarfile
+import time
+import traceback
 from dataclasses import dataclass
-from multiprocessing import JoinableQueue, Process
+from io import BytesIO
+from multiprocessing import JoinableQueue, Process, Queue
 from pathlib import Path
 from typing import List
 
@@ -21,6 +25,13 @@ class Chunk:
     source: str
     index: int
     items: List[FileCuts]
+
+
+@dataclass
+class TarChunk:
+    source: str
+    index: int
+    items: List[Chunk]
 
 
 def build_chunks(files, max_sec):
@@ -69,11 +80,13 @@ class SimpleAudioWriter:
     def __init__(self, output_dir, sample_rate):
         self.output_dir = output_dir
         self.sample_rate = sample_rate
+        logger.info(f"Initialized SimpleAudioWriter {output_dir} with sample rate {sample_rate}")
 
     def write(self, name, data):
         output_file = Path(self.output_dir) / name
         os.makedirs(output_file.parent, exist_ok=True)
-        sf.write(output_file, data, self.sample_rate, subtype="PCM_16")
+        with open(output_file, "wb") as f:
+            f.write(data.getbuffer())
         logger.debug(f"Saved {output_file}")
 
 
@@ -99,14 +112,39 @@ class S3AudioWriter:
             files.append(obj["Key"])
         return files
 
-    def write(self, name: Path, data):
-        from io import BytesIO
+    def write(self, name: str, data):
+        start = time.perf_counter()
+        size_mb = len(data.getbuffer()) / 1024 / 1024
         output_file = name
-        buffer = BytesIO()
-        sf.write(buffer, data, self.sample_rate, subtype="PCM_16", format="WAV")
-        buffer.seek(0)
-        self.s3_client.upload_fileobj(buffer, self.bucket, str(output_file))
-        logger.debug(f"Saved {output_file} to bucket {self.bucket}")
+        logger.debug(f"Starting upload {output_file} {size_mb:.2f} MB")
+        self.s3_client.upload_fileobj(data, self.bucket, str(output_file))
+        elapsed = time.perf_counter() - start
+        logger.debug(f"Uploaded {output_file}: {size_mb:.2f} MB in {elapsed:.2f}s ({size_mb / elapsed:.2f} MB/s)")
+
+
+def calc_time(chunk: TarChunk):
+    res = 0
+    for chunk in chunk.items:
+        for fc in chunk.items:
+            for seg in fc.segments:
+                res += seg.end - seg.start
+    return res
+
+
+def build_tar(param, n):
+    items, source, index = [], None, 0
+    for c in param:
+        if len(items) >= n or c.source != source:
+            if len(items) > 0:
+                yield TarChunk(source=source, index=index, items=items)
+            if c.source != source:
+                source, index = c.source, 0
+            else:
+                index += 1
+            items = []
+        items.append(c)
+    if len(items) > 0:
+        yield TarChunk(source=source, index=index, items=items)
 
 
 def main(argv):
@@ -139,7 +177,7 @@ def main(argv):
 
     files = []
     with open(args.input, "r", encoding="utf-8") as f:
-        for i, line in enumerate(tqdm(f)):
+        for i, line in enumerate(tqdm(f, desc="Load segments")):
             # if i > 10000:
             #     break
             data_dict = json.loads(line)
@@ -150,24 +188,38 @@ def main(argv):
     logger.info(f"loaded {len(files)} sorting")
     files.sort(key=lambda s: (s.source, s.path), )
     tot_secs = 0
-    for f in tqdm(files):
+    for f in tqdm(files, desc="Calculate total audio seconds"):
         for s in f.segments:
             tot_secs += s.end - s.start
     logger.info(f"Total audio seconds: {tot_secs} ({tot_secs / 3600:.2f} hours)")
 
     logger.info(f"Start copying audio with {args.workers} workers")
     workers = []
-    work_queue = JoinableQueue(maxsize=100)
+    work_queue = JoinableQueue(maxsize=1)
+    error_queue = Queue(maxsize=args.workers)
     for i in range(args.workers):
-        p = Process(target=worker, args=(work_queue, args, file_write))
+        p = Process(target=worker, args=(work_queue, args, file_write, error_queue))
         p.start()
         workers.append(p)
 
-    with tqdm(total=len(files), desc="Copying") as pbar:
-        for chunk in build_chunks(files, args.audio_seconds):
-            fc = len(chunk.items)
-            work_queue.put(chunk)
-            pbar.update(fc)
+    skip = True
+    with tqdm(total=tot_secs / 3600, desc="Copying", unit="h",
+              bar_format="{l_bar}{bar}| "
+                         "{n:.2f}/{total:.2f} {unit} "
+                         "[{elapsed}<{remaining}, {rate_fmt}]") as pbar:
+        for chunk in build_tar(build_chunks(files, args.audio_seconds), 30):
+            if not error_queue.empty():
+                logger.error("Error in worker")
+                break
+            fc = calc_time(chunk)
+            # # crawl_034745.wav
+            # for c in chunk:
+            if chunk.source == "08kHz" and chunk.index == 120:
+                skip = False
+                logger.info(f"Continue from {chunk.source} {chunk.index}")
+            if not skip:
+                work_queue.put(chunk)
+            pbar.update(fc / 3600)
 
     for _ in workers:
         work_queue.put(None)
@@ -175,51 +227,85 @@ def main(argv):
     for p in workers:
         p.join()
 
+    while not error_queue.empty():
+        logger.error(f"Error in worker: {error_queue.get()}")
+
     logger.info(f"Done")
 
 
-def worker(work_queue, args, file_writer):
+class AudioReader:
+    def __init__(self):
+        self.path = None
+        self.data = None
+        self.sr = None
+
+    def read(self, path):
+        if self.path != path:
+            # logger.debug(f"open file {path}")
+            self.data, self.sr = sf.read(path)
+            self.path = path
+
+        return self.data, self.sr
+
+
+def worker(work_queue, args, file_writer, error_queue):
     logger.info("Worker started")
 
     base_audio_path = Path(args.audio_base)
 
-    while True:
-        chunk = work_queue.get()
+    ar = AudioReader()
 
-        if chunk is None:
+    while True:
+        tar_chunk: TarChunk = work_queue.get()
+
+        if tar_chunk is None:
             logger.info("Worker exiting")
             break
-
+        tar_name = f"{tar_chunk.source}/{tar_chunk.index:06d}.tar"
         try:
-            bucket_id = chunk.index // 1000
-            output_file = Path(chunk.source) / f"{bucket_id:03d}" / f"{chunk.source}_{chunk.index:06d}.wav"
-            parts = []
-            for fc in chunk.items:
-                a_path = Path(fc.path)
-                if not a_path.is_absolute():
-                    a_path = base_audio_path / a_path / "audio.16.wav"
-                # logger.info(f"loading {a_path}")
-                data, sr = sf.read(a_path)
-                if sr != args.sample_rate:
-                    raise ValueError(f"Sample rate mismatch: {sr} != {args.sample_rate}")
+            logger.debug(f"got task {tar_name} with {len(tar_chunk.items)} chunks")
 
-                for seg in fc.segments:
-                    start = int(seg.start * sr)
-                    end = int(seg.end * sr)
-                    parts.append(data[start:end])
+            tar_buffer = BytesIO()
+            with tarfile.open(fileobj=tar_buffer, mode="w:") as tar:
+                for chunk in tar_chunk.items:
+                    output_file = f"{chunk.source}/{chunk.index:06d}.wav"
+                    parts = []
+                    for fc in chunk.items:
+                        a_path = Path(fc.path)
+                        if not a_path.is_absolute():
+                            a_path = base_audio_path / a_path / "audio.16.wav"
+                        # logger.info(f"loading {a_path}")
+                        data, sr = ar.read(a_path)
+                        if sr != args.sample_rate:
+                            raise ValueError(f"Sample rate mismatch: {sr} != {args.sample_rate}")
 
-            if not parts:
-                logger.warning(f"Empty chunk {output_file}")
-                continue
+                        for seg in fc.segments:
+                            start = int(seg.start * sr)
+                            end = int(seg.end * sr)
+                            parts.append(data[start:end])
 
-            combined = np.concatenate(parts)
+                    if not parts:
+                        logger.warning(f"Empty chunk {output_file}")
+                        continue
 
-            file_writer.write(output_file, combined)
+                    combined = np.concatenate(parts)
+                    buffer = BytesIO()
+                    sf.write(buffer, combined, sr, subtype="PCM_16", format="WAV")
+                    buffer.seek(0)
 
-            logger.debug(f"Saved {output_file}")
+                    info = tarfile.TarInfo(name=output_file)
+                    info.size = len(combined)
+                    tar.addfile(tarinfo=info, fileobj=buffer)
+
+            tar_buffer.seek(0)
+            file_writer.write(name=tar_name, data=tar_buffer)
+
+            logger.debug(f"Saved {tar_name}")
 
         except Exception as e:
-            logger.exception(f"Error processing chunk {chunk.source}_{chunk.index}: {e}")
+            tb = traceback.format_exc()
+            error_queue.put(f"error in {tar_name}: {tb}")
+            return
 
         finally:
             work_queue.task_done()
